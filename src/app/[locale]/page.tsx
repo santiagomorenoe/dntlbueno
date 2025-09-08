29c22c6c094f9fd66e057576e9056192514d835c
santiagomorenoe <morenoestradasantiago@gmail.com>
2025-09-07 21:45:44 -0600

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,13 +14,14 @@ export default function Home() {
   return (
     <div className="min-h-screen relative">
       {/* leyenda del desarrollador */}
-      <div className="text-center text-xs text-foreground bg-background/90 backdrop-blur-sm sticky top-24 z-50 w-fit mx-auto">
+      <div className="text-center text-xs bg-background/50 py-2 text-foreground backdrop-blur-3xl sticky top-32 md:top-24 z-50 w-fit px-12 mx-auto">
         <p className="italic">
           Producto de demostración. Hecho por {" "}
           <a href="/images/cv_santiago_moreno.pdf" target="_blank" className="text-primary underline ">
             Santi Moreno
           </a>{" "}
-          con ❤️
+          con ❤️ Para 
+          <a href="https://dentalbueno.mx/" target="_blank" className="text-primary underline"> Dental Bueno 🦷</a>
         </p>
       </div>
       <Navbar />
